5ca120bad34df6596a4e9252688142d68e852469
lauix <lauixData@gmail.com>
2019-07-04 14:18:26 +0800

    修复滚动条问题

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -16,9 +16,11 @@
     height: 100%;
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
     overflow-y: hidden;
+    overflow-x: hidden;
   }
 
   #app{
     height: 100%;
   }
 </style>
+
